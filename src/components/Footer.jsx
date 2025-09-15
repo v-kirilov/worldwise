c3355831b6de077f87c66980a90622017d40f113
v-kirilov <v_kirilov87@protonmail.ch>
2025-09-15 12:32:25 +0000

    Add styles , move files to be used. Create a map and sidebar components.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          {" "}
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,13 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>This is the app layout</p>
+    <div className={styles.appLayout}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
