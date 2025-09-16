81928a8e09965353f7c85ae4b922903440e7b502
v-kirilov <v_kirilov87@protonmail.ch>
2025-09-16 11:43:53 +0000

    Reading and setting a query string.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -9,10 +9,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date,id ,position} = city;
   return (
     <li>
-      <Link className={styles.cityItem} to={`${city.id}`}>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
