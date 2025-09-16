2cf56550c985e9ee8ed1448e35b3c1c687b8d918
v-kirilov <v_kirilov87@protonmail.ch>
2025-09-16 13:55:35 +0000

    Programmatic navigation with useNavigate.

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -23,7 +23,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} key={country.emoji} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,22 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   console.log(lat, lng);
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
-      <h1>Position: {lat}, {lng}</h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 55 })}>Change position</button>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 55 })}>
+        Change position
+      </button>
     </div>
   );
 }
