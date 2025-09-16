0240e0c2cb3808718b1b2d7128cc6531ad023827
v-kirilov <v_kirilov87@protonmail.ch>
2025-09-16 08:25:00 +0000

    Implement cities list!

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,32 @@ import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading the data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -15,8 +39,14 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>Default list of index</p>} />
-          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import Message from "./Message";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!cities.length) {
+    return <Message message="Add your first city by clicking on a city in the map!"/>;
+  }
+  return (
+    <ul className={styles.cityList}>
+      {cities.map(city=> (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+CityList.propTypes = {
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+export default CityList;
